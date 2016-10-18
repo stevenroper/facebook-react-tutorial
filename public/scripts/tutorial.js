858122a1e6ec685e014680028d525d462135580e
Steven Roper <stevenroper@StevenRoper.local>
2016-10-17 21:23:30 -0600

    Break out components.

--- public/scripts/tutorial.js
+++ public/scripts/tutorial.js
@@ -1,8 +1,30 @@
+var CommentList = React.createClass({
+  render: function () {
+    return (
+      <div className='commentList'>
+        Hey. I'm a comment list.
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function () {
+    return (
+      <div className='commentForm'>
+        Yo. Comment form here.
+      </div>
+    );
+  }
+});
+
 var CommentBox = React.createClass({
   render: function () {
     return (
       <div className="commentBox">
-        Sup world. I'm a effing comment box.
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
       </div>
     );
   }
