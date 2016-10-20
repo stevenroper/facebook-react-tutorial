1af9e9e1d524807fec3539b80e929dbbc944083e
Steven Roper <stevenroper@StevenRoper.local>
2016-10-20 16:14:12 -0600

    Finish app.

--- public/scripts/tutorial.js
+++ public/scripts/tutorial.js
@@ -92,7 +92,18 @@ var CommentBox = React.createClass({
     });
   },
   handleCommentSubmit: function(comment) {
-    // Add server submit logic here.
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: comment,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
   },
   componentDidMount: function() {
     this.loadCommentsFromServer();
